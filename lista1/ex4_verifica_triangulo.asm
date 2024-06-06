9e92442d28e520d00326f14183768bb3f02d26bd
Pedro Venâncio dos Santos <89045459+pdrVenancio@users.noreply.github.com>
2024-06-06 13:01:09 -0300

    Exercício de triangulo finalizado

--- lista1/ex4_verifica_triangulo.asm
+++ lista1/ex4_verifica_triangulo.asm
@@ -1,4 +1,8 @@
 .data
+equ: 	.asciiz "equilatero"
+iso: 	.asciiz "isoceles"
+esc: 	.asciiz "escaleno"
+
 .text
 .globl main
 main:
@@ -18,4 +22,39 @@ main:
 	add $s3, $zero, $v0 # s3 = l3
 	
 	
+	beq $s1, $s2, ver_equilatero # compara se dois lados
+	j ver_isoceles
 	
+	#verifica se é equilatero
+	ver_equilatero:
+		beq $s1, $s3, equilatero # compara o ultimo lado 
+		j isoceles
+		
+	ver_isoceles:
+		beq $s1, $s3, isoceles # compara o ultimo lado
+		j ver_s2 # se s1 e s2 forem diferntes aida tenho que compara s com s2  
+		
+		ver_s2:
+			beq $s2, $s3, isoceles #comparo os lado que falta 
+			j escaleno
+		
+	equilatero:
+		#imprimir
+		li $v0, 4 
+		la $a0, equ
+		syscall
+		j fim
+	isoceles:
+		#imprimir
+		li $v0,4
+		la $a0, iso
+		syscall
+		j fim
+	escaleno:
+		#imprimir
+		li $v0, 4
+		la $a0, esc
+		syscall
+		j fim
+		
+	fim:
