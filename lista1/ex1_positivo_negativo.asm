15ae8dfe603e1c30660ac6ca60e99ef360086253
pdrVenancio <pedrosantos121229@gmail.com>
2024-06-07 20:42:09 -0300

    atiavidade 1

--- lista1/ex1_positivo_negativo.asm
+++ lista1/ex1_positivo_negativo.asm
@@ -9,24 +9,24 @@ main:
 	#addi $t0,$zero, 10
 	
 	# ler um valor inteiro
-	li $v0, 5	#codigo para ler um inteiro
-	syscall 	#executa o comando so para ler
+	li $v0, 5	# codigo para ler um inteiro
+	syscall 	# executa o comando so para ler
 	
-	la $t0, 0($v0)	#passa o valor lido para t0
+	la $t0, 0($v0)	# passa o valor lido para t0
 	
 	slt $t1, $t0, $zero # t0 < zero true t1 = 1 else t1 = 0
 	
-	add $t2 , $zero, 1 #atribui t2 como 1
-	beq $t1, $t2, then #verifica se t1 e t2 sao iguais
+	add $t2 , $zero, 1 # atribui t2 como 1
+	beq $t1, $t2, then # verifica se t1 e t2 sao iguais
 	j else
 then:
-	li $v0, 4 	#imprimir a string
-	la $a0, neg	#passa o valor de negativo para ser impresso
+	li $v0, 4 	# imprimir a string
+	la $a0, neg	# passa o valor de negativo para ser impresso
 	syscall
 	j fim
 else:
-	li $v0, 4 	#imprimir a string
-	la $a0, pos	#passa o valor de positivo para ser impresso
+	li $v0, 4 	# imprimir a string
+	la $a0, pos	# passa o valor de positivo para ser impresso
 	syscall
 	j fim
 
